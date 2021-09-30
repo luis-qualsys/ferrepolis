4d249e61ec132f6f635bdd185ad48950770551e3
Luis Eloy Homobono Fuentes <luis_homobono@qualsys.com.mx>
2021-09-30 16:59:06 -0500

    [NEW] Módulo POS to MRP

---
+++ fer_pos_mrp_order/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'MRP orden de POS',
+    'version': '13.0.1.0.3',
+    'summary': """Generar una MRP orden despues de vender en POS.""",
+    'description': """Generar una MRP orden despues de vender en POS.""",
+    'author': 'Qualsys Consulting',
+    'company': 'Qualsys Consulting',
+    'website': 'https://www.qualsys.com.mx',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale', 'mrp', 'stock'],
+    # 'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_view.xml',
+        'views/pos_template.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+}
